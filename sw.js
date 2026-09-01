82e49ef2fa8938f12bad17fa4aa35d89077c8bd2
ffouad198319-hue <ffouad198319@gmail.com>
2026-09-01 21:35:56 +0300

    Refresh plan visibility update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cleancontrol-v8-20260901-schedules';
+const CACHE='cleancontrol-v9-20260901-supervisor-plans';
 const SHELL=['./','./index.html','./core.js','./rounds.js','./notes.js','./admin.js','./schedule-ui.js','./reports.js','./boot.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

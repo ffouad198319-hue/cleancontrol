2831c2372acdc17aa3fc2e2abf0b71aee68eeead
ffouad198319-hue <ffouad198319@gmail.com>
2026-09-01 22:14:19 +0300

    Refresh multi-floor plan cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cleancontrol-v10-20260901-workplans';
+const CACHE='cleancontrol-v11-20260901-multifloor';
 const SHELL=['./','./index.html','./core.js','./rounds.js','./notes.js','./admin.js','./schedule-ui.js','./reports.js','./boot.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
